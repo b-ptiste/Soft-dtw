ee44efe829aef90558ca1565ae328b84ed17ab0c
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2023-12-12 19:26:25 +0100

    Add batch manipulation + Add back-propagation

--- code/dtw_soft.py
+++ code/dtw_soft.py
@@ -1,4 +1,6 @@
+# third-party library
 import torch
+
 
 def soft_min(list_a, gamma):
     """Softmin function.
@@ -11,10 +13,10 @@ def soft_min(list_a, gamma):
         float: softmin value
     """
     assert gamma >= 0, "gamma must be greater than or equal to 0"
-    
+
     # transform list_a to numpy array
     list_a = torch.Tensor(list_a)
-    
+
     if gamma == 0:
         _min = torch.min(list_a)
     else:
@@ -24,36 +26,113 @@ def soft_min(list_a, gamma):
     return _min
 
 
-def soft_dtw(x, y, gamma=1.0):
+def soft_dtw(
+    x: torch.Tensor, y: torch.Tensor, gamma: float = 1.0
+) -> tuple[torch.Tensor, torch.Tensor]:
     """Soft Dynamic Time Warping.
 
     Args:
-        x (list): time series 1
-        y (list): time series 2
+        x (list): length x feature
+        y (list): length x feature
         gamma (float, optional): gamma parameter. Defaults to 1.0.
 
     Returns:
         float: soft-DTW distance
     """
     # initialize DP matrix
-    n = len(x)
-    m = len(y)
+    n = x.shape[0]
+    m = y.shape[0]
     R = torch.zeros((n + 1, m + 1))
-    R[0, 1:] = float('inf')
-    R[1:, 0] = float('inf')
+    R[0, 1:] = float("inf")
+    R[1:, 0] = float("inf")
     R[0, 0] = 0.0
-    
+
+    try:
+        cost = torch.cdist(x, y, p=2) ** 2
+    except:
+        print(
+            "Carefull : x and y are not D-dimensional > 1 features : added 2 dimensions"
+        )
+        cost = torch.cdist(x.unsqueeze(1), y.unsqueeze(1), p=2) ** 2
+
     for j in range(1, m + 1):
         for i in range(1, n + 1):
-            # calculate distance
-            cost = (x[i - 1] - y[j - 1])**2
-            
             # calculate minimum
             _min = soft_min([R[i - 1, j], R[i, j - 1], R[i - 1, j - 1]], gamma)
-            
+
             # update cell
-            R[i, j] = cost + _min
-            
+            R[i, j] = cost[i - 1, j - 1] + _min
+
     return R[-1, -1], R
-    
-    
+
+
+def soft_dtw_batch(x: torch.Tensor, y: torch.Tensor, gamma: float = 1) -> torch.Tensor:
+    """Soft Dynamic Time Warping.
+
+    Args:
+        x (list): Channel x length x feature
+        y (list): Channel x length x feature
+        gamma (float, optional): gamma parameter. Defaults to 1.0.
+
+    Returns:
+        float: soft-DTW distance
+    """
+    batch = x.shape[0]
+    dists = torch.zeros(batch)
+    for i in range(batch):
+        x_i = x[i]
+        y_i = y[i]
+        dist, _ = soft_dtw(x_i, y_i, gamma)
+        dists[i] = dist
+
+    return dists
+
+
+def backward_recursion(
+    x: torch.Tensor, y: torch.Tensor, gamma: float = 1.0
+) -> torch.Tensor:
+    """backward recursion of soft-DTW
+
+    Args:
+        x (torch.Tensor): length x feature
+        y (torch.Tensor): length x feature
+        gamma (float, optional): gamma parameter. Defaults to 1.0.
+
+    Returns:
+        torch.Tensor: E matrix
+    """
+    n, m = x.shape[0], y.shape[0]
+    # intialization
+
+    # compute delta
+    try:
+        delta = torch.cdist(x, y, p=2) ** 2
+    except:
+        print(
+            "Carefull : x and y are not D-dimensional > 1 features : added 2 dimensions"
+        )
+        delta = torch.cdist(x.unsqueeze(1), y.unsqueeze(1), p=2) ** 2
+    # delta[:-1, m], delta[n, :-1] = 0.0, 0.0
+    delta = torch.cat((delta, torch.zeros((n)).reshape(-1, 1)), dim=1)
+    delta = torch.cat((delta, torch.zeros((m + 1)).reshape(1, -1)), dim=0)
+    delta[n, m] = 0.0
+
+    # compute E
+    E = torch.zeros((n + 2, m + 2))
+    E[n + 1, m + 1] = 1.0
+
+    # compute R
+    _, R = soft_dtw(x, y, gamma=gamma)
+    R = torch.cat((R, -float("inf") * torch.ones((n + 1)).reshape(-1, 1)), dim=1)
+    R = torch.cat((R, -float("inf") * torch.ones((m + 2)).reshape(1, -1)), dim=0)
+    R[n + 1, m + 1] = R[n, m]
+
+    # backward recursion
+    for j in range(m, 0, -1):  # ranges from m to 1
+        for i in range(n, 0, -1):  # ranges from n to 1
+            a = torch.exp((R[i + 1, j] - R[i, j] - delta[i, j - 1]) / gamma)
+            b = torch.exp((R[i, j + 1] - R[i, j] - delta[i - 1, j]) / gamma)
+            c = torch.exp((R[i + 1, j + 1] - R[i, j] - delta[i, j]) / gamma)
+            E[i, j] = E[i + 1, j] * a + E[i, j + 1] * b + E[i + 1, j + 1] * c
+
+    return E[1:-1, 1:-1]
