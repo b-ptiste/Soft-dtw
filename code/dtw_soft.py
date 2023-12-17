9464be7266d8ed5bde3f964f8483ed23c069c027
Adhemar de Senneville <adhemar.senneville@gmail.com>
2023-12-18 00:17:35 +0100

    batch_bacward

--- code/dtw_soft.py
+++ code/dtw_soft.py
@@ -173,9 +173,10 @@ def soft_dtw_batch_same_size(x: torch.Tensor, y: torch.Tensor, gamma: float = 1)
     """
     # initialize DP matrix
     n = x.shape[1]
+    m = y.shape[1]
     b = x.shape[0]
 
-    R = torch.zeros((b, n + 1, n + 1))
+    R = torch.zeros((b, n + 1, m + 1))
     R[:, 0, 1:] = float("inf")
     R[:, 1:, 0] = float("inf")
     R[:, 0, 0] = 0.0
@@ -189,7 +190,58 @@ def soft_dtw_batch_same_size(x: torch.Tensor, y: torch.Tensor, gamma: float = 1)
         cost = torch.cdist(x.unsqueeze(1), y.unsqueeze(1), p=2) ** 2
 
     for i in range(1, n + 1):
-        for j in range(1, n + 1):
+        for j in range(1, m + 1):
             R[:, i, j] = cost[:, i - 1, j - 1] + soft_min_batch([R[:, i - 1, j], R[:, i, j - 1], R[:, i - 1, j - 1]], gamma)
 
-    return R[:, -1, -1]
+    return R[:, -1, -1], R
+
+
+def backward_recursion_batch_same_size(
+    x: torch.Tensor, y: torch.Tensor, gamma: float = 1.0
+) -> torch.Tensor:
+    """backward recursion of soft-DTW
+
+    Args:
+        x (torch.Tensor): batch x length x feature
+        y (torch.Tensor): batch x length x feature
+        gamma (float, optional): gamma parameter. Defaults to 1.0.
+
+    Returns:
+        torch.Tensor: E matrix
+    """
+    batch = x.shape[0]
+    n, m = x.shape[1], y.shape[1]
+    # intialization
+
+    # compute delta
+    try:
+        delta = torch.cdist(x, y, p=2) ** 2
+    except:
+        print(
+            "Carefull : x and y are not D-dimensional > 1 features : added 2 dimensions"
+        )
+        delta = torch.cdist(x.unsqueeze(1), y.unsqueeze(1), p=2) ** 2
+    # delta[:-1, m], delta[n, :-1] = 0.0, 0.0
+    delta = torch.cat((delta, torch.zeros((batch,n)).reshape(batch, -1, 1)), dim=2)
+    delta = torch.cat((delta, torch.zeros((batch,m + 1)).reshape(batch, 1, -1)), dim=1)
+    delta[:, n, m] = 0.0
+
+    # compute E
+    E = torch.zeros((batch, n + 2, m + 2))
+    E[:, n + 1, m + 1] = 1.0
+
+    # compute R
+    _, R = soft_dtw_batch_same_size(x, y, gamma=gamma)
+    R = torch.cat((R, -float("inf") * torch.ones((batch,n + 1)).reshape(batch, -1, 1)), dim=2)
+    R = torch.cat((R, -float("inf") * torch.ones((batch,m + 2)).reshape(batch, 1, -1)), dim=1)
+    R[:, n + 1, m + 1] = R[:, n, m]
+
+    # backward recursion
+    for j in range(m, 0, -1):  # ranges from m to 1
+        for i in range(n, 0, -1):  # ranges from n to 1
+            a = torch.exp((R[:, i + 1, j] - R[:, i, j] - delta[:, i, j - 1]) / gamma)
+            b = torch.exp((R[:, i, j + 1] - R[:, i, j] - delta[:, i - 1, j]) / gamma)
+            c = torch.exp((R[:, i + 1, j + 1] - R[:, i, j] - delta[:, i, j]) / gamma)
+            E[:, i, j] = E[:, i + 1, j] * a + E[:, i, j + 1] * b + E[:, i + 1, j + 1] * c
+
+    return E[:, 1:-1, 1:-1]
